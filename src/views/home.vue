131fbf535e7ea306b329c93dbd2ed1f87f7c403c
hammann <hammann@qq.com>
2026-05-06 14:30:22 +0800

    fix:图片路径错误

--- src/views/home.vue
+++ src/views/home.vue
@@ -176,9 +176,9 @@ const pageInfoList = ref([
 
 // ========== 4. 同盟链接数据 ==========
 const allianceList = ref([
-  { imageSrc: "`${baseUrl}images/omelet2.png`", alt: "蛋包饭", text: "★蛋包饭真爱同盟★" ,path: "/omelet-alliance"},
-  { imageSrc: "`${baseUrl}images/mentako.png`", alt: "面蛸", text: "★面蛸同盟★" ,path: "/mentako-alliance"},
-  { imageSrc: "`${baseUrl}images/moon.png`", alt: "★月球", text: "★想回月球联盟★" ,path: "/moon-alliance"},
+  { imageSrc: `${baseUrl}images/omelet2.png`, alt: "蛋包饭", text: "★蛋包饭真爱同盟★" ,path: "/omelet-alliance"},
+  { imageSrc: `${baseUrl}images/mentako.png`, alt: "面蛸", text: "★面蛸同盟★" ,path: "/mentako-alliance"},
+  { imageSrc: `${baseUrl}images/moon.png`, alt: "★月球", text: "★想回月球联盟★" ,path: "/moon-alliance"},
 ]);
 </script>
 
